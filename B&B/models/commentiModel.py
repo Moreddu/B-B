4c2cd8e0f572aaeeb2834b9a653ef6f7699309ab
Moreddu <moreddugiovanni@gmail.com>
2019-05-17 10:26:55 +0200

    prova aggiunta controllers

---
+++ B&B/models/commentiModel.py
@@ -0,0 +1,9 @@
+from google.appengine.ext import ndb
+
+class Commento(ndb.Model):
+    commento_id= ndb.StringProperty(required=True)
+    guest_name= ndb.StringProperty()
+    guest_email=ndb.IntegerProperty(required=True)
+    guest_picture=ndb.StringProperty()
+    test=ndb.StringProperty()
+    datetime=ndb.DateTimeProperty(auto_now=True)
